8854016abe1d5a649dcae17f831e6fa3aa1b02e5
Sharique Hasan <>
2020-05-09 04:26:40 +0200

    Add configuration interfaces

---
+++ src/interfaces/IConfiguration.ts
@@ -0,0 +1,18 @@
+export interface IConfiguration {
+    id: string;
+    fullyQualifiedName: string;
+    label: string[];
+    version: number;
+    name: string;
+    path: string;
+    description: string;
+    type: string;
+    data: string;
+}
+
+export interface IConfigurationResponse {
+    success: boolean,
+    error: string | null;
+    count: number;
+    data: IConfiguration[];
+}
