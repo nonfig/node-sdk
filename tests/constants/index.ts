e5248c50807b2e78a6d807af3a0839002d80e756
Sharique Hasan <>
2020-05-09 04:27:23 +0200

    Add constants for test

---
+++ tests/constants/index.ts
@@ -0,0 +1,23 @@
+import {IConnection, IOption} from "../../src/interfaces";
+
+const DEFAULT_HOST = 'localhost';
+const DEFAULT_PORT = 443;
+const DEFAULT_BASE_PATH = '/';
+const DEFAULT_API_VERSION = 'v1';
+const DEFAULT_TIMEOUT = 80000;
+
+export const connection: IConnection = {
+    host: DEFAULT_HOST,
+    port: DEFAULT_PORT,
+    basePath: DEFAULT_BASE_PATH,
+    apiVersion: DEFAULT_API_VERSION,
+    timeout: DEFAULT_TIMEOUT
+};
+
+export const options: IOption = {
+    appId: "",
+    appSecret: "",
+    debug: false,
+    cacheEnable: false,
+    cacheTtl: 1000
+}
