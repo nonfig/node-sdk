2b53f17e9959027f89672da56dc9b047ff5193fd
Hamza Waqas <ArkeologeN@users.noreply.github.com>
2020-05-20 11:41:48 +0200

    fix: added .npmignore

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const DEFAULT_BASE_PATH = '/';
 const DEFAULT_API_VERSION = 'v1';
 const DEFAULT_TIMEOUT = 80000;
 
-module.exports =  function nonfig(options: IOption) {
+module.exports = function nonfig(options: IOption) {
     const connection: IConnection = {
         host: DEFAULT_HOST,
         port: DEFAULT_PORT,
@@ -17,4 +17,4 @@ module.exports =  function nonfig(options: IOption) {
     };
 
     return new Api(connection, options);
-}
+};
