b43c2d6a26d6a3481e59ccdbd10c1cdc70b46ac5
Sharique Hasan <>
2020-05-20 14:17:39 +0200

    fix typings and tests

--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
-import { IConnection, IOption } from "./src/interfaces";
-import Api from "./src/api";
+import { IConnection, IOption } from './src/interfaces';
+import Api from './src/api';
 
-const DEFAULT_HOST = "api.nonfig.com";
+const DEFAULT_HOST = 'api.nonfig.com';
 const DEFAULT_PORT = 443;
-const DEFAULT_BASE_PATH = "/";
-const DEFAULT_API_VERSION = "v1";
+const DEFAULT_BASE_PATH = '/';
+const DEFAULT_API_VERSION = 'v1';
 const DEFAULT_TIMEOUT = 80000;
 
 export default function nonfig(options: IOption): Api {
-  const connection: IConnection = {
-    host: DEFAULT_HOST,
-    port: DEFAULT_PORT,
-    basePath: DEFAULT_BASE_PATH,
-    apiVersion: DEFAULT_API_VERSION,
-    timeout: DEFAULT_TIMEOUT
-  };
+    const connection: IConnection = {
+        host: DEFAULT_HOST,
+        port: DEFAULT_PORT,
+        basePath: DEFAULT_BASE_PATH,
+        apiVersion: DEFAULT_API_VERSION,
+        timeout: DEFAULT_TIMEOUT,
+    };
 
-  return new Api(connection, options);
-};
+    return new Api(connection, options);
+}
