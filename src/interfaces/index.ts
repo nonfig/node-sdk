b882682b2322cf48777b0285c20e0d47c38827ad
Sharique Hasan <>
2020-12-17 13:34:42 +0500

    chore: expose interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,4 +2,5 @@ export * from './IOption';
 export * from './IConnection';
 export * from './IConfiguration';
 export * from './IHeaders';
+export * from './IRequestOption';
 export * from './Nonfig';
