6163cc3dd3e0d24abe3b028ddb5ce6cc3ab2c9c0
Sharique Hasan <>
2020-12-17 13:44:23 +0500

    chore: expose methods

--- src/interfaces/Nonfig.ts
+++ src/interfaces/Nonfig.ts
@@ -1,8 +1,9 @@
 import { Configuration } from '../configuration.entity';
+import {IRequestOption} from "./IRequestOption";
 
 export interface Nonfig {
-    findByPath(path: string): Promise<Configuration[]>;
-    findByName(name: string): Promise<Configuration[]>;
-    findById(id: string): Promise<Configuration[]>;
-    findByLabels(labels: string[]): Promise<Configuration[]>;
+    findByPath(path: string, options?: IRequestOption): Promise<Configuration[]>;
+    findByName(name: string, options?: IRequestOption): Promise<Configuration[]>;
+    findById(id: string, options?: IRequestOption): Promise<Configuration[]>;
+    findByLabels(labels: string[], options?: IRequestOption): Promise<Configuration[]>;
 }
